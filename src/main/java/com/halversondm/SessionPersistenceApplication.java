3e4b14bc8b5ccec24449b3c4ede717179c5b8f29
Dan Halverson <daniel.m.halverson@gmail.com>
2021-12-12 13:49:09 -0600

    initial commit

---
+++ src/main/java/com/halversondm/SessionPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.halversondm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halversondm/SessionPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halversondm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
