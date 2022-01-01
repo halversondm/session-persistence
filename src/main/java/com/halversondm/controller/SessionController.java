be581bfbf09bfab3f01f53597b588c91e9a3a32c
Dan Halverson <daniel.m.halverson@gmail.com>
2022-01-01 10:17:25 -0600

    use java 17 fully

--- src/main/java/com/halversondm/controller/SessionController.java
+++ src/main/java/com/halversondm/controller/SessionController.java
@@ -1,8 +1,6 @@
 package com.halversondm.controller;
 
 import com.halversondm.dto.AppSessionData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping(path = "/api")
 public class SessionController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SessionController.class);
 
     @GetMapping(path = "/data")
     public AppSessionData establishSession(HttpServletRequest httpServletRequest) {
--- src/main/java/com/halversondm/filter/SessionFilter.java
+++ src/main/java/com/halversondm/filter/SessionFilter.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -21,7 +20,6 @@ public class SessionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpSession session = request.getSession(false);
 
         if (request.getRequestURI().contains("actuator")) {
